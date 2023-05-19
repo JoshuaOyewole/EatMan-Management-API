37bd60ee4efda2c42e2d5666d8add785e8517a8d
joshuaOyewole <joshpp2013@gmail.com>
2023-05-19 06:44:52 +0100

    verifyToken modified

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,21 +4,23 @@ const createError = require("../util/error");
 const verifyToken = (req, res, next) => {
   //check if the user even have a token. If not throw an error
   let token;
+
   if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers?.authorization !== undefined &&
+    req.headers?.authorization?.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers?.authorization?.split(" ")[1];
     //verify if a token is valid. If not valid throw an error
     jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
-      //if valid access the data in the token in a req variable
-      if (err)
-        return next(
-          createError(403, "Access denied, Incorrect Authorization Code")
-        );
 
-      req.user = data;
-      next();
+      //if valid access the data in the token in a req variable
+      if (!err) {
+        req.user = data;
+        return next();
+      } else {
+        return createError(403, "Access denied, Incorrect Authorization Code"
+        );
+      }
     });
   } else {
     return next(createError(401, "Access denied, No Authorization code"));
@@ -26,10 +28,8 @@ const verifyToken = (req, res, next) => {
 };
 
 const verifyUser = (req, res, next) => {
-
   verifyToken(req, res, () => {
-
-    if (req.user.id === req.params.id || req.user.rank === 'user' ) {
+    if (req.user.id === req.params.id || req.user.rank === "user" || "admin") {
       next();
     } else {
       return next(createError(403, "You are not authorized!"));
@@ -37,10 +37,9 @@ const verifyUser = (req, res, next) => {
   });
 };
 const verifyStaff = (req, res, next) => {
-
   verifyToken(req, res, () => {
 
-    if (req.user.id === req.params.id || (req.user.rank === 'staff' || 'admin')) {
+    if (req.user.id === req.params.id || req.user.rank === "staff" || "admin") {
       next();
     } else {
       return next(createError(403, "You are not authorized!"));
@@ -50,8 +49,7 @@ const verifyStaff = (req, res, next) => {
 
 const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-
-    if (req.user.rank === 'admin') {
+    if (req.user.rank === "admin") {
       next();
     } else {
       return next(createError(403, "You are not authorized!"));
