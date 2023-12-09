b610337dbf0cad4ea6d6be16175f4bbd27fdde2a
joshua Oyewole <joshpp2013@gmail.com>
2023-12-09 15:33:52 +0100

    added totalSale

--- controllers/salesRecord.js
+++ controllers/salesRecord.js
@@ -15,19 +15,52 @@ const eod = async (req, res, next) => {
 
 /* LAST 7 days Transactions */
 const last7days = async (req, res, next) => {
-
   try {
-    const data = await Transaction.find({ timestamp: {
-      $gte: new Date(new Date() - 6 * 60 * 60 * 24 * 1000)
-  }});
+    const data = await Transaction.find({
+      timestamp: {
+        $gte: new Date(new Date() - 6 * 60 * 60 * 24 * 1000),
+      },
+    });
     res.status(200).json(data);
   } catch (error) {
     return next(createError(400, error.message));
   }
 };
 
+/* TOTAL SALES */
+const totalSale = async (req, res, next) => {
+  try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 
-/* const getTransactionByDate = async (req, res) => {
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999); // Set time to the end of the day
+
+    const total = await Transaction.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: today,
+            $lte: endOfDay,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalPrice: { $sum: "${totalPrice}" },
+        },
+      },
+    ]);
+
+    res.json({ total: total.length > 0 ? total[0].totalPrice : 0 });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* 
+const getTransactionByDate = async (req, res) => {
   try {
     //get dates from req.query by es6 object destructuring
 
@@ -79,4 +112,4 @@ const last7days = async (req, res, next) => {
   } //Catch Ends here
 }; */
 
-module.exports = { eod,last7days };
+module.exports = { eod, last7days, totalSale };
--- routes/record/index.js
+++ routes/record/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {
     eod,
-    last7days
+    last7days,
+    totalSale
 } = require("../../controllers/salesRecord");
 const { verifyStaff } = require('../../middleware/verifyToken');
 
@@ -10,5 +11,7 @@ const { verifyStaff } = require('../../middleware/verifyToken');
 router.get('/', verifyStaff , eod);
 router.get('/last7days',verifyStaff, last7days);
 
+/* DASHBOARD */
+router.get('/totalSale', verifyStaff , totalSale);
 
 module.exports = router;
